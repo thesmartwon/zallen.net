bae178e663942004210b1dc17541f839ac6cc978
thesmartwon <thesmartwon@pm.me>
2025-04-25 16:48:12 -0400

    fix post order

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,4 +1,4 @@
 export default {
-	"/posts/zig-good": await import("./01-zig-good.md"),
 	"/posts/zig-bad": await import("./02-zig-bad.md"),
+	"/posts/zig-good": await import("./01-zig-good.md"),
 };
